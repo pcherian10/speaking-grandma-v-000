ca3ee418d22f3cb1a6f70c18c6ba4b4f4a37babd
Paul <pcherian10@gmail.com>
2018-04-30 18:15:09 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,8 @@
 
 def speak_to_grandma (phrase)
   return "I LOVE YOU TOO PUMPKIN!" if phrase == "I LOVE YOU GRANDMA!"
-  return "NO"
+  return "NO, NOT SINCE 1938!" if phrase == phrase.upcase()
+  
   
   
   
